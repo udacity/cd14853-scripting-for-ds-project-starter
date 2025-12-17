ca9387a3941e784730fb7c31cd61c509c8650dc9
kedar-k <kedarkodgire.kk@gmail.com>
2025-12-17 17:03:49 -0400

    starter files

---
+++ starter/run_pipeline.sh
@@ -0,0 +1,55 @@
+!/bin/bash
+#
+# run_pipeline.sh
+#
+# Purpose:
+# Run the full data processing pipeline from start to finish.
+#
+# NOTE:
+# This file contains starter code only.
+# You are expected to complete the TODO sections.
+
+# TODO:
+# Print a message indicating that the pipeline has started.
+#
+# echo "..."
+
+# TODO:
+# Print a message indicating that the filename organization step is starting.
+#
+# echo "..."
+
+# TODO:
+# Run the organize_files.sh script.
+#
+
+# TODO:
+# Print a message indicating that the validation step is starting.
+#
+# echo "..."
+
+# TODO:
+# Run the validate_and_route.sh script.
+#
+
+# TODO:
+# Print a message indicating that the JSON processing step is starting.
+#
+# echo "..."
+
+# TODO:
+# Run the process_jsons.py script.
+
+
+# TODO:
+# Print a message indicating that dataset merging is starting.
+#
+# echo "..."
+
+# TODO:
+# Run the merge_to_dataset.py script.
+
+# TODO:
+# Print a message indicating that the pipeline has completed successfully.
+#
+# echo "..."
